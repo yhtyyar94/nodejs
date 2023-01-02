e09df725c9ad0cefe26ac5fc363b00ecefe2ccc7
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2023-01-02 18:24:23 +0100

    test

--- controllers/telegram/telegram.js
+++ controllers/telegram/telegram.js
@@ -1,9 +1,8 @@
-const TelegramBot = require("node-telegram-bot-api");
+const bot = require("../../config/telegram");
 
 //1194064413
 
 exports.runBot = async (req, res) => {
-  const bot = new TelegramBot(process.env.Telegram, { polling: true });
   const commands = [
     {
       command: "/sor",
